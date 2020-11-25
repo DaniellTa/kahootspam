a9c6a447c8574efcc89d6a49ed059d3c9db2afcb
DaniellTa <61436073+DaniellTa@users.noreply.github.com>
2020-11-25 17:35:14 -0500

    Update kahootspam.py

--- kahootspam.py
+++ kahootspam.py
@@ -10,14 +10,14 @@ import threading
 if len(sys.argv) != 3:
     gamepin = input('Game PIN: ')
     numofthreads = int(input('# of bots: '))
+    name = input("Nickname: ")
 else:
     numofthreads = int(sys.argv[2])
     gamepin = sys.argv[1]
-    
+
 
 options = ChromeOptions()
 options.add_argument('headless')
-#numofthreads = int(sys.argv[2])
 driver_count = 0
 pin_count = 0
 username_count = 0
@@ -29,20 +29,19 @@ def new_game(i):
     global driver_count, pin_count, username_count, done_count
     driver = Chrome(ChromeDriverManager().install(), chrome_options=options)
     driver.get('https://kahoot.it/')
-    driver_count+= 1
-    sleep(3)
+    driver_count += 1
     try:
         pinning = driver.find_element_by_xpath('//*[@id="game-input"]')
         pinning.send_keys(gamepin)
         pinning.send_keys(Keys.ENTER)
-        pin_count+= 1
-        sleep(3)
+        pin_count += 1
+        sleep(1)
         nick = driver.find_element_by_xpath(
             '//*[@id="nickname"]')
-        username_count+= 1
-        nick.send_keys(str(random.randint(0, 1000000000)))
+        username_count += 1
+        nick.send_keys(name + str(username_count))
         nick.send_keys(Keys.ENTER)
-        done_count+= 1
+        done_count += 1
     except TypeError:
         print("Thread #%i has errored!" % i)
 
@@ -57,16 +56,12 @@ spawn(numofthreads)
 print()
 
 while driver_count < numofthreads:
-    sleep(1)
     print("\r[%i/%i] Starting webdriver" % (driver_count, numofthreads), end='')
 while pin_count < numofthreads:
-    sleep(1)
     print("\r[%i/%i] Entering game pin." % (pin_count, numofthreads), end='')
 while username_count < numofthreads:
-    sleep(1)
     print("\r[%i/%i] Entering in nickname" % (username_count, numofthreads), end='')
 while done_count < numofthreads:
-    sleep(1)
     print("\r%i/%i threads done.      " % (done_count, numofthreads), end='')
 
 input('\n[*] Press enter to exit')
